a6d6e7cab49c9d562a5bf2e13cae2f69bb5f0002
Tsertsvadze3 <shota.tsertsvadze@gau.edu.ge>
2024-10-03 21:32:35 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1
+function applyStyle() {
+    const textElement = document.getElementById('styledText');
+    textElement.style.fontStyle = 'italic';
+    textElement.style.textDecoration = 'underline';
+}
+
+// 2
+
+function jami(){
+    let x = 5;
+    let y = 7;
+    let z = x + y
+    console.log(z)
+}
+
+jami()
+
+// 3
+
+function demo(){
+    let txt = 'teqsti'
+    const textElement = document.getElementById('styledText');
+    textElement.style.fontSize = '100px';
+}
+
+demo()
